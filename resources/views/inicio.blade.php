2a98ee4da0ebf00b07ee3e1e5b9530b1a2928da0
emancilla01 <emancilla01@gmail.com>
2025-10-14 22:28:57 -0500

    undid changes

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,3 +1,14 @@
+{{-- @extends('plantillas.plantilla2')
+@section('menu2')
+    @include('menu2')
+@endsection
+@section('contenido2')
+<div class="d-flex justify-content-center align-items-center" style="height: 80vh;">
+    <h3>Sistema web que permite gestionar de forma eficiente el registro, control y 
+        seguimiento de productos, clientes, proveedores y ventas.</h3>
+</div>
+@endsection  --}}
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
